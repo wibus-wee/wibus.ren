9a1dc346518e67c1652f3becb50a7ee3224ac306
Wibus <62133302+wibus-wee@users.noreply.github.com>
2022-08-27 11:44:17 +0800

    fix: remove useless deps

--- components/widgets/Plum.tsx
+++ components/widgets/Plum.tsx
@@ -6,10 +6,7 @@
  * @LastEditTime: 2022-08-26 23:44:35
  * Coding With IU
  */
-import React from 'react'
-import { ref, computed, onMounted, reactive } from 'vue'
-import { defineComponent } from 'reactivue'
-import { Fn, useRafFn, useWindowSize } from '@vueuse/core'
+
 
 const Plum = () => {
   return (
