241b1f5d78fb168c4efc12b9b073e6de056d110c
wibus-wee <1596355173@qq.com>
2022-10-07 20:36:54 +0800

    feat: `<Plum />` in app

--- hooks/use-raf-fn.ts
+++ hooks/use-raf-fn.ts
@@ -3,22 +3,43 @@
  * @author: Wibus
  * @Date: 2022-08-26 17:47:29
  * @LastEditors: Wibus
- * @LastEditTime: 2022-08-26 17:47:29
+ * @LastEditTime: 2022-10-07 20:34:04
  * Coding With IU
  */
 
-import { useState, useEffect } from "react";
+import * as React from 'react';
 
+type Fn = () => void;
 
-export const useRafFn = (fn: () => void) => {
-  const [rafId, setRafId] = useState(0);
-  useEffect(() => {
-    const id = requestAnimationFrame(() => {
-      fn();
-      setRafId(id);
+export function useRafFn(fn: Fn) {
+  const isActive = React.useRef(false);
+  let rafId: null | number = null;
+
+  const loop = () => {
+    if (!isActive.current || !window) return;
+
+    fn();
+    rafId = window.requestAnimationFrame(loop);
+  };
+
+  const resume = () => {
+    if (!isActive.current && window) {
+      isActive.current = true;
+      loop();
     }
-    );
-    return () => cancelAnimationFrame(id);
-  }
-  , [rafId]);
+  };
+
+  const pause = () => {
+    isActive.current = false;
+    if (rafId != null && window) {
+      window.cancelAnimationFrame(rafId);
+      rafId = null;
+    }
+  };
+
+  return {
+    isActive,
+    pause,
+    resume,
+  };
 }
