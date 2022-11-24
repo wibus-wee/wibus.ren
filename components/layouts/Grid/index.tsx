21b284fc7c2c587465cffba73f135b83294ac70e
wibus-wee <1596355173@qq.com>
2022-11-24 19:27:44 +0800

    perf: grid layout

--- components/layouts/Grid/index.tsx
+++ components/layouts/Grid/index.tsx
@@ -3,19 +3,21 @@
  * @author: Wibus
  * @Date: 2022-11-24 13:41:53
  * @LastEditors: Wibus
- * @LastEditTime: 2022-11-24 13:46:35
+ * @LastEditTime: 2022-11-24 18:29:11
  * Coding With IU
  */
 import clsx from "clsx"
 import styles from "./index.module.css"
+import { motion } from "framer-motion"
 
 export function Grid({ children }) {
   return <div className={styles.grid}>{children}</div>
 }
 
-export function GridItem({ cstart, cend, rstart, rend, about, children }) {
+export function GridItem({ cstart, cend, rstart, rend, about = false, children }) {
   return (
-    <div className={clsx(styles.gridItem, about && styles.about)}
+    <div 
+      className={clsx(styles.gridItem, about && styles.about)}
       style={{
         gridColumnStart: cstart,
         gridColumnEnd: cend,
