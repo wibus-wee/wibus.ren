f76d1116270d494804b6963c7f82d6cf38eaaca0
wibus-wee <1596355173@qq.com>
2022-11-25 17:04:41 +0800

    chore: add pre-commit git hook

--- components/widgets/Twitter/index.tsx
+++ components/widgets/Twitter/index.tsx
@@ -3,55 +3,59 @@
  * @author: Wibus
  * @Date: 2022-11-24 18:36:41
  * @LastEditors: Wibus
- * @LastEditTime: 2022-11-25 16:23:35
+ * @LastEditTime: 2022-11-25 16:28:50
  * Coding With IU
  */
-import clsx from "clsx"
-import { useState } from "react";
-import { Play, play } from "../../../utils/play.util"
-import styles from "./index.module.css"
-import { motion } from "framer-motion"
+import clsx from 'clsx'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
+
 import config from '@contents/config.json'
-import Markdown from "../Markdown";
+
+import { Play, play } from '../../../utils/play.util'
+import styles from './index.module.css'
 
 export const Twitter = () => {
-
-  const defaultColor = "#000"
-  const [scale, setScale] = useState<number>(1);
-  const [color, setColor] = useState<string>(defaultColor);
-  const [grayColor, setGrayColor] = useState<string>("#949495");
-
+  const defaultColor = '#000'
+  const [scale, setScale] = useState<number>(1)
+  const [color, setColor] = useState<string>(defaultColor)
+  const [grayColor, setGrayColor] = useState<string>('#949495')
 
   return (
     <div className={clsx(styles.container)}>
       <div>
         <div className={clsx(styles.innerContainer)}>
-          <a className={clsx(styles.info)} href={config.status.userlink} >
+          <a className={clsx(styles.info)} href={config.status.userlink}>
             <div className={clsx(styles.avatar)}>
               <img src={config.status.avatar} alt={config.status.username} />
             </div>
-            <motion.div className={clsx(styles.nameContainer)}
+            <motion.div
+              className={clsx(styles.nameContainer)}
               animate={{ color }}
               transition={{ duration: 0.3 }}
             >
               <div className={clsx(styles.name)}>{config.status.nickname}</div>
-              <motion.div className={clsx(styles.username)}
+              <motion.div
+                className={clsx(styles.username)}
                 animate={{ color: grayColor }}
                 transition={{ duration: 0.3 }}
-              >@{config.status.username}</motion.div>
+              >
+                @{config.status.username}
+              </motion.div>
             </motion.div>
           </a>
           <div className={clsx(styles.icon)}>
             <a href={config.status.link} className={clsx(styles.link)}>
               <img src={config.status.icon} alt="icon" />
             </a>
-            <motion.div className={clsx(styles.iconBg)}
+            <motion.div
+              className={clsx(styles.iconBg)}
               animate={{
                 scale,
               }}
               transition={{
                 duration: 0.3,
-                ease: "easeInOut",
+                ease: 'easeInOut',
               }}
               style={{
                 backgroundColor: config.status.bgColor,
@@ -59,7 +63,8 @@ export const Twitter = () => {
             />
           </div>
         </div>
-        <motion.div className={clsx(styles.content)}
+        <motion.div
+          className={clsx(styles.content)}
           animate={{ color }}
           transition={{ duration: 0.3 }}
           dangerouslySetInnerHTML={{ __html: config.status.content }}
@@ -68,14 +73,14 @@ export const Twitter = () => {
 
       <TwitterReadBtn
         onMouseEnter={() => {
-          setScale(40);
-          setColor("#fff");
-          setGrayColor("#dfdfdf");
+          setScale(40)
+          setColor('#fff')
+          setGrayColor('#dfdfdf')
         }}
         onMouseLeave={() => {
-          setScale(1);
-          setColor(defaultColor);
-          setGrayColor("#949495");
+          setScale(1)
+          setColor(defaultColor)
+          setGrayColor('#949495')
         }}
         color={color}
         href={config.status.link}
@@ -85,9 +90,19 @@ export const Twitter = () => {
   )
 }
 
-export const TwitterReadBtn = ({ href, text, onMouseEnter, onMouseLeave, color }) => {
+export const TwitterReadBtn = ({
+  href,
+  text,
+  onMouseEnter,
+  onMouseLeave,
+  color,
+}) => {
   return (
-    <a className={clsx(styles["btn"])} href={href} target="_blank" rel="noreferrer"
+    <a
+      className={clsx(styles['btn'])}
+      href={href}
+      target="_blank"
+      rel="noreferrer"
       onMouseEnter={() => {
         play(Play.twitterSound)
         onMouseEnter()
@@ -97,10 +112,13 @@ export const TwitterReadBtn = ({ href, text, onMouseEnter, onMouseLeave, color }
       }}
     >
       <div className={clsx(styles.inner)}>
-        <motion.p className={clsx(styles.text)}
+        <motion.p
+          className={clsx(styles.text)}
           animate={{ color }}
           transition={{ duration: 0.3 }}
-        >{text}</motion.p>
+        >
+          {text}
+        </motion.p>
       </div>
     </a>
   )
