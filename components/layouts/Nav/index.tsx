ae81acb819377f331e1644f8bdc1dd13868a03a3
wibus-wee <1596355173@qq.com>
2022-11-25 17:19:58 +0800

    perf: indicator translate with router path

--- components/layouts/Nav/index.tsx
+++ components/layouts/Nav/index.tsx
@@ -3,49 +3,65 @@
  * @author: Wibus
  * @Date: 2022-11-23 13:39:59
  * @LastEditors: Wibus
- * @LastEditTime: 2022-11-24 13:54:30
+ * @LastEditTime: 2022-11-25 17:17:44
  * Coding With IU
  */
-import clsx from "clsx";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useMemo, useState } from "react";
-import styles from "./index.module.css";
-import { motion } from "framer-motion";
+import clsx from 'clsx'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useMemo, useState } from 'react'
+
 import config from '../../../contents/config.json'
-import { Play, play } from "../../../utils/play.util";
+import { Play, play } from '../../../utils/play.util'
+import styles from './index.module.css'
 
 export const Nav = () => {
   const router = useRouter()
   const pathMatch = useMemo(() => {
-    const asPath = router.asPath;
-    const path = asPath.split("/")[1];
-    return path;
-  }, [router.asPath]);
-  const [translatePresent, setTranslatePresent] = useState(0);
+    const asPath = router.asPath
+    const path = asPath.split('/')[1]
+    return path
+  }, [router.asPath])
+  const [translatePresent, setTranslatePresent] = useState(
+    config.nav.findIndex((item) => item.path === pathMatch) * 100,
+  )
+
   const handleIndicator = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    setTranslatePresent(Number(e.currentTarget.dataset.index) * 100);
+    setTranslatePresent(Number(e.currentTarget.dataset.index) * 100)
     play(Play.navClick)
   }
-
 
   return (
     <div className={clsx(styles.container)}>
       <div className={styles.nav}>
-        <motion.a id="indicator" href="#" className={clsx(styles["floating-indicator"])} animate={{ x: `${translatePresent}%` }} />
-        {
-          config.nav.map((item, index) => {
-            return (
-              <Link href={`/${item.path}`} key={index} className={clsx(styles["nav-item"], "nav-item")} data-index={index} onClick={handleIndicator}>
-                <div className={clsx(styles["nav-text"], pathMatch === item.path && styles["active"])}>
-                  {item.text}
-                </div>
-              </Link>
-            )
-          })
-        }
-
+        <motion.a
+          id="indicator"
+          href="#"
+          className={clsx(styles['floating-indicator'])}
+          animate={{ x: `${translatePresent}%` }}
+        />
+        {config.nav.map((item, index) => {
+          return (
+            <Link
+              href={`/${item.path}`}
+              key={index}
+              className={clsx(styles['nav-item'], 'nav-item')}
+              data-index={index}
+              onClick={handleIndicator}
+            >
+              <div
+                className={clsx(
+                  styles['nav-text'],
+                  pathMatch === item.path && !item.external && styles['active'],
+                )}
+              >
+                {item.text}
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
-};
+}
