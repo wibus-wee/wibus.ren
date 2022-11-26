a9294a3195a8c2f1bc67a220c04a83c69bd8121c
wibus-wee <1596355173@qq.com>
2022-11-26 16:24:55 +0800

    feat: umami track events

---
+++ constants/events.ts
@@ -0,0 +1,16 @@
+/*
+ * @FilePath: /iucky.cn/constants/events.ts
+ * @author: Wibus
+ * @Date: 2022-11-26 16:08:59
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-11-26 16:20:23
+ * Coding With IU
+ */
+
+export enum Events {
+  clickNavItem = '点击了导航栏的项目',
+  clickPhotosMenu = '点击了照片的菜单',
+  clickPhotos = '点击了照片架',
+  enterStatusWidget = '鼠标移入了状态小部件',
+  clickContentLink = '点击了内容链接',
+}
---
+++ utils/umami.util.ts
@@ -0,0 +1,14 @@
+/*
+ * @FilePath: /iucky.cn/utils/umami.util.ts
+ * @author: Wibus
+ * @Date: 2022-11-26 16:14:57
+ * @LastEditors: Wibus
+ * @LastEditTime: 2022-11-26 16:14:57
+ * Coding With IU
+ */
+
+export const createUmamiEvent = (event: string) => {
+  if (typeof window !== 'undefined') {
+    window.umami.trackEvent(event)
+  }
+}
