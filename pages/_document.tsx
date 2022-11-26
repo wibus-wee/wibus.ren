4cda428bb64cc03e30f129eed3f405012b7b05c0
wibus-wee <1596355173@qq.com>
2022-11-26 16:32:02 +0800

    perf: using config to config umami script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,18 +3,18 @@
  * @author: Wibus
  * @Date: 2022-08-07 21:00:31
  * @LastEditors: Wibus
- * @LastEditTime: 2022-09-12 22:16:10
+ * @LastEditTime: 2022-11-26 16:31:33
  * Coding With IU
  */
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-import Document, { Head, Html, Main, NextScript } from "next/document";
-
+import config from '@contents/config.json'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head lang={"zh-cn"}>
+        <Head lang={'zh-cn'}>
           <meta charSet="UTF-8" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -24,19 +24,21 @@ export default class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#ffffff" />
           <link rel="icon" type="image/png" href="/favicon.png" />
-          <script async defer data-website-id="ec4a742f-f950-4d87-94ea-d57920decb44" src="https://umami.iucky.cn/umami.js"></script>
-
+          <script
+            async
+            defer
+            data-website-id={config.umami.id}
+            src={config.umami.src}
+          />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <link rel="apple-touch-startup-image" href="/favicon.png" />
-
         </Head>
-
 
         <body id="app" className="font-sans text-gray-700 dark:text-gray-200">
           <Main />
           <NextScript />
         </body>
-      </Html >
+      </Html>
     )
   }
 }
