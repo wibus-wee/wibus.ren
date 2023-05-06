4e31864d48c9517f3d5c8d5aa3a61356c6c11d97
wibus-wee <1596355173@qq.com>
2023-05-06 09:50:13 +0800

    refactor(Grid): grid layout

--- components/layouts/Grid/index.tsx
+++ components/layouts/Grid/index.tsx
@@ -6,24 +6,55 @@
  * @LastEditTime: 2022-11-25 14:22:59
  * Coding With IU
  */
-import clsx from "clsx"
-import styles from "./index.module.css"
-import { motion } from "framer-motion"
+import clsx from 'clsx'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+
+import styles from './index.module.css'
 
 export function Grid({ children, ...props }) {
-  return <div className={styles.grid} {...props} >{children}</div>
+  return (
+    <div className={styles.grid} {...props}>
+      {children}
+    </div>
+  )
 }
 
-export function GridItem({ cstart, cend, rstart, rend, about = false, children }) {
+export function Flex({ children, ...props }) {
   return (
-    <div 
-      className={clsx(styles.gridItem, about && styles.about)}
-      style={{
-        gridColumnStart: cstart,
-        gridColumnEnd: cend,
-        gridRowStart: rstart,
-        gridRowEnd: rend,
-      }}
-    >{children}</div>
+    <div className={styles.flex} {...props}>
+      {children}
+    </div>
+  )
+}
+
+export function GridItem({
+  large = false,
+  superLarge = false,
+  centered = false,
+  children,
+  lightOnly = false,
+  className = '',
+  index,
+  ...props
+}) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true, margin: '-20px' }}
+      transition={{ duration: Math.min(0.25 + index * 0.2, 0.8) }}
+      className={clsx(
+        styles.gridItem,
+        large && styles.large,
+        centered && styles.centered,
+        lightOnly && styles['light-only'],
+        className,
+        superLarge && styles.superLarge,
+      )}
+      {...props}
+    >
+      {children}
+    </motion.div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { readFile } from 'fs/promises'
 import type { NextPage } from 'next'
 import { NextSeo } from 'next-seo'
-import { Grid, GridItem } from '../components/layouts/Grid'
+
+import { Flex, Grid, GridItem } from '../components/layouts/Grid'
 import Markdown from '../components/widgets/Markdown'
 import { Photos } from '../components/widgets/Photos'
 import { Twitter } from '../components/widgets/Twitter'
-
 
 export const getStaticProps = async () => {
   const content = await readFile('./contents/readme.md', 'utf8')
@@ -19,30 +19,28 @@ export const getStaticProps = async () => {
 }
 
 const Home: NextPage = (props: any) => {
-
   return (
-    <div className='h-screen'>
-      <NextSeo
-        title={"Wibus"}
-        description={"Just Uaeua"}
-      />
+    <div className="h-screen">
+      <NextSeo title={'Wibus'} description={'Just Uaeua'} />
       <Grid>
-        <GridItem cstart={1} cend={2} rstart={1} rend={3} about>
+        <GridItem index={0} large>
           <article>
             <div className="prose m-auto">
               <Markdown content={props.content} />
             </div>
           </article>
         </GridItem>
-        <GridItem cstart={2} cend={3} rstart={1} rend={2}>
-          <Twitter />
-        </GridItem>
-        <GridItem cstart={3} cend={4} rstart={1} rend={2} >
-          <Photos />
-        </GridItem>
-        <GridItem cstart={2} cend={4} rstart={2} rend={3} >
+        <Flex>
+          <GridItem index={1}>
+            <Twitter />
+          </GridItem>
+          <GridItem index={2}>
+            <Photos />
+          </GridItem>
+        </Flex>
+        <GridItem index={3} superLarge>
           <article>
-            <div className="prose others m-auto">
+            <div className="prose others max-w-[100%]">
               <Markdown content={props.others} />
             </div>
           </article>
